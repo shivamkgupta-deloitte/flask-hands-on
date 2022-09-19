da6402b59842922d9e8f80dab107eebbe2fe7dd7
Gupta_ Shivam Kumar (shivamkugupta@deloitte.com) <shivamkugupta@your.hostname.com>
2022-09-19 10:11:32 +0530

    added delete endpoint

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'Shivam'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)          #automatically creates /auth endpoint
 
 
 items=[]
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return { 'item' : item }, 200 if item else 404
@@ -32,6 +38,11 @@ class Item(Resource):
             item = { 'name' : name, 'price' : data['price']}
             items.append(item)
             return item
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return { 'Message' : 'Item deleted!' }
 
 class ItemList(Resource):
     def get(self):
