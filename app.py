94e397246dfc4e6d2f65b605aadb2d1dc195b545
Gupta_ Shivam Kumar (shivamkugupta@deloitte.com) <shivamkugupta@your.hostname.com>
2022-09-19 11:45:26 +0530

    optimized code using reuest parser

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ items=[]
 
 
 class Item(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -24,20 +32,13 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return { 'message' : "An item with name '{}' already exists".format(name)}, 404
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = { 'name' : name, 'price' : data['price'] }
         items.append(item)
         return item, 201
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-            type=float,
-            required=True,
-            help="This field cannot be left blank!"
-        )
-
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = { 'name' : name, 'price' : data['price'] }
